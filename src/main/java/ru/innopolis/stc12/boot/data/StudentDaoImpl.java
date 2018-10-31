9dae7dc7b8d209dbf6826bac7eeb41dfba5c42de
MVBiryukov-stc11 <m.biryukov@innopolis.ru>
2018-10-31 20:22:23 +0300

    Updated jdbc

--- src/main/java/ru/innopolis/stc12/boot/data/StudentDaoImpl.java
+++ src/main/java/ru/innopolis/stc12/boot/data/StudentDaoImpl.java
@@ -2,55 +2,56 @@ package ru.innopolis.stc12.boot.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.innopolis.stc12.boot.data.mapper.StudentMapper;
 import ru.innopolis.stc12.boot.data.pojo.Student;
 
 import java.util.List;
 
 @Repository
-public class StudentDaoImpl implements StudentDao {
-    private JdbcTemplate jdbcTemplate;
-
-    public StudentDaoImpl() {
+@Transactional
+public class StudentDaoImpl extends JdbcDaoSupport implements StudentDao {
+    @Autowired
+    public StudentDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.setJdbcTemplate(jdbcTemplate);
     }
 
-    @Autowired
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public StudentDaoImpl() {
     }
 
     @Override
     public List<Student> getStudentsList() {
         String getStudentsQuery = "select * from students";
-        return jdbcTemplate.query(getStudentsQuery, new StudentMapper());
+        return super.getJdbcTemplate().query(getStudentsQuery, new StudentMapper());
     }
 
     @Override
     public void addStudent(Student student) {
         String addStudentQuery = "insert into students (name, family_name, age, contact)" +
                 " values (?,?,?,?)";
-        jdbcTemplate.update(addStudentQuery, student.getName(), student.getFamilyName(),
+        super.getJdbcTemplate().update(addStudentQuery, student.getName(), student.getFamilyName(),
                 student.getAge(), student.getGroup());
     }
 
     @Override
     public Student getStudent(Integer id) {
         String getStudentQuery = "select * from students where id=?";
-        return jdbcTemplate.queryForObject(getStudentQuery,
+        return super.getJdbcTemplate().queryForObject(getStudentQuery,
                 new Object[]{id}, new StudentMapper());
     }
 
     @Override
     public void deleteStudent(Integer id) {
         String deleteStudentQuery = "delete from students where id=?";
-        jdbcTemplate.update(deleteStudentQuery, id);
+        super.getJdbcTemplate().update(deleteStudentQuery, id);
     }
 
     @Override
     public void update(Integer id, Integer age, String name, String familyName) {
         String updateStudentQuery = "update students set name=?, family_name=?, age=? " +
                 "where id=?";
-        jdbcTemplate.update(updateStudentQuery, name, familyName, age, id);
+        super.getJdbcTemplate().update(updateStudentQuery, name, familyName, age, id);
     }
 }
